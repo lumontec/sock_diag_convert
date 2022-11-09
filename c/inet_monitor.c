2d65bd2f57634199fc5aa74938b9c3f16c9e4e4f
lumontec <lucamontechiesi@gmail.com>
2022-11-08 21:15:17 -0800

    Update inet_monitor: support periodic dump of sockets

--- c/inet_monitor.c
+++ c/inet_monitor.c
@@ -19,6 +19,7 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
@@ -70,121 +71,54 @@ static const char* tcp_states_map[]={
 //Copied from libmnl source
 #define SOCKET_BUFFER_SIZE (getpagesize() < 8192L ? getpagesize() : 8192L)
 
-//Example of diag_filtering, checks if destination port is <= 1000
-//
-//The easies way to understand filters, is to look at how the kernel
-//processes them. This is done in the function inet_diag_bc_run() in
-//inet_diag.c. The yes/no contains offsets to the next condition or aborts
-//the loop by making the variable len in inet_diag_bc_run() negative. There
-//are some limitations to the yes/no values, see inet_diag_bc_audit();
-unsigned char create_filter(void **filter_mem){
-    struct inet_diag_bc_op *bc_op = NULL;
-    unsigned char filter_len = sizeof(struct inet_diag_bc_op)*2;
-    if((*filter_mem = calloc(filter_len, 1)) == NULL)
+static int send_query(int fd)
+{
+    struct sockaddr_nl nladdr = {
+        .nl_family = AF_NETLINK
+    };
+    struct
+    {
+        struct nlmsghdr nlh;
+        struct inet_diag_req_v2 idr;
+    } req = {
+        .nlh = {
+            .nlmsg_len = sizeof(req),
+            .nlmsg_type = SOCK_DIAG_BY_FAMILY,
+            .nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP
+        },
+        .idr = {
+            .sdiag_family = AF_INET,
+            .sdiag_protocol = IPPROTO_TCP,
+            .idiag_states = -1,
+//            .idiag_states = TCPF_ALL & 
+//                ~((1<<TCP_SYN_RECV) | (1<<TCP_TIME_WAIT) | (1<<TCP_CLOSE)),
+            .idiag_ext = -1,
+        }
+    };
+    struct iovec iov = {
+        .iov_base = &req,
+        .iov_len = sizeof(req)
+    };
+    struct msghdr msg = {
+        .msg_name = (void *) &nladdr,
+        .msg_namelen = sizeof(nladdr),
+        .msg_iov = &iov,
+        .msg_iovlen = 1
+    };
+
+    for (;;) {
+        if (sendmsg(fd, &msg, 0) < 0) {
+            if (errno == EINTR)
+                continue;
+
+            perror("sendmsg");
+            return -1;
+        }
+
         return 0;
-
-    bc_op = (struct inet_diag_bc_op*) *filter_mem; 
-    bc_op->code = INET_DIAG_BC_D_LE;
-    bc_op->yes = sizeof(struct inet_diag_bc_op)*2;
-    //Only way to stop loop is to make len negative
-    bc_op->no = 12;
-
-    //For a port check, the port to check for is stored in the no field of a
-    //follow-up bc_op-struct.
-    bc_op = bc_op+1;
-    bc_op->no = 1000;
-
-    return filter_len;
-}
-
-int send_diag_msg(int sockfd){
-    struct msghdr msg;
-    struct nlmsghdr nlh;
-    //To request information about unix sockets, this would be replaced with
-    //unix_diag_req, packet-sockets packet_diag_req.
-    struct inet_diag_req_v2 conn_req;
-    struct sockaddr_nl sa;
-    struct iovec iov[4];
-    int retval = 0;
-
-    //For the filter
-    struct rtattr rta;
-    void *filter_mem = NULL;
-    int filter_len = 0;
-
-    memset(&msg, 0, sizeof(msg));
-    memset(&sa, 0, sizeof(sa));
-    memset(&nlh, 0, sizeof(nlh));
-    memset(&conn_req, 0, sizeof(conn_req));
-
-    //No need to specify groups or pid. This message only has one receiver and
-    //pid 0 is kernel
-    sa.nl_family = AF_NETLINK;
-
-    //Address family and protocol we are interested in. sock_diag can also be 
-    //used with UDP sockets, DCCP sockets and Unix sockets, to mention a few.
-    //This example requests information about TCP sockets bound to IPv4
-    //addresses.
-    conn_req.sdiag_family = AF_INET;
-    conn_req.sdiag_protocol = IPPROTO_TCP;
-
-    //Filter out some states, to show how it is done
-//    conn_req.idiag_states = TCPF_ALL & 
-//        ~((1<<TCP_SYN_RECV) | (1<<TCP_TIME_WAIT) | (1<<TCP_CLOSE));
-    conn_req.idiag_states = -1;
-
-
-
-    //Request extended TCP information (it is the tcp_info struct)
-    //ext is a bitmask containing the extensions I want to acquire. The values
-    //are defined in inet_diag.h (the INET_DIAG_*-constants).
-    conn_req.idiag_ext |= (1 << (INET_DIAG_INFO - 1));
-    
-    nlh.nlmsg_len = NLMSG_LENGTH(sizeof(conn_req));
-    //In order to request a socket bound to a specific IP/port, remove
-    //NLM_F_DUMP and specify the required information in conn_req.id
-    nlh.nlmsg_flags = NLM_F_DUMP | NLM_F_REQUEST;
-
-    //Example of how to only match some sockets
-    //In order to match a single socket, I have to provide all fields
-    //sport/dport, saddr/daddr (look at dump_on_icsk)
-    //conn_req.id.idiag_dport=htons(443);
-
-    //Avoid using compat by specifying family + protocol in header
-    nlh.nlmsg_type = SOCK_DIAG_BY_FAMILY;
-    iov[0].iov_base = (void*) &nlh;
-    iov[0].iov_len = sizeof(nlh);
-    iov[1].iov_base = (void*) &conn_req;
-    iov[1].iov_len = sizeof(conn_req);
-
-    //Remove the if 0 to test the filter
-#if 0
-    if((filter_len = create_filter(&filter_mem)) > 0){
-        memset(&rta, 0, sizeof(rta));
-        rta.rta_type = INET_DIAG_REQ_BYTECODE;
-        rta.rta_len = RTA_LENGTH(filter_len);
-        iov[2] = (struct iovec){&rta, sizeof(rta)};
-        iov[3] = (struct iovec){filter_mem, filter_len};
-        nlh.nlmsg_len += rta.rta_len;
     }
-#endif
-
-    //Set essage correctly
-    msg.msg_name = (void*) &sa;
-    msg.msg_namelen = sizeof(sa);
-    msg.msg_iov = iov;
-    if(filter_mem == NULL)
-        msg.msg_iovlen = 2;
-    else
-        msg.msg_iovlen = 4;
-   
-    retval = sendmsg(sockfd, &msg, 0);
-
-    if(filter_mem != NULL)
-        free(filter_mem);
-
-    return retval;
 }
+
 
 void parse_diag_msg(struct inet_diag_msg *diag_msg, int rtalen){
     struct rtattr *attr;
@@ -238,6 +172,7 @@ void parse_diag_msg(struct inet_diag_msg *diag_msg, int rtalen){
                         remote_addr_buf, ntohs(diag_msg->id.idiag_dport),
                         tcpi->tcpi_retrans
                         );
+                fflush(stdout);
 
 //                //Output some sample data
 //                fprintf(stdout, 
@@ -260,6 +195,75 @@ void parse_diag_msg(struct inet_diag_msg *diag_msg, int rtalen){
     }
 }
 
+
+static int receive_responses(int fd)
+{
+    long buf[8192 / sizeof(long)];
+    struct sockaddr_nl nladdr = {
+        .nl_family = AF_NETLINK
+    };
+    struct iovec iov = {
+        .iov_base = buf,
+        .iov_len = sizeof(buf)
+    };
+    int flags = 0;
+
+    for (;;) {
+        struct msghdr msg = {
+            .msg_name = (void *) &nladdr,
+            .msg_namelen = sizeof(nladdr),
+            .msg_iov = &iov,
+            .msg_iovlen = 1
+        };
+
+        ssize_t ret = recvmsg(fd, &msg, flags);
+
+        if (ret < 0) {
+            if (errno == EINTR)
+                continue;
+
+            perror("recvmsg");
+            return -1;
+        }
+        if (ret == 0)
+            return 0;
+
+        const struct nlmsghdr *h = (struct nlmsghdr *) buf;
+
+        if (!NLMSG_OK(h, ret)) {
+            fputs("!NLMSG_OK\n", stderr);
+            return -1;
+        }
+
+        for (; NLMSG_OK(h, ret); h = NLMSG_NEXT(h, ret)) {
+            if (h->nlmsg_type == NLMSG_DONE)
+                return 0;
+
+            if (h->nlmsg_type == NLMSG_ERROR) {
+                const struct nlmsgerr *err = NLMSG_DATA(h);
+
+                if (h->nlmsg_len < NLMSG_LENGTH(sizeof(*err))) {
+                    fputs("NLMSG_ERROR\n", stderr);
+                } else {
+                    errno = -err->error;
+                    perror("NLMSG_ERROR");
+                }
+
+                return -1;
+            }
+
+            if (h->nlmsg_type != SOCK_DIAG_BY_FAMILY) {
+                fprintf(stderr, "unexpected nlmsg_type %u\n",
+                        (unsigned) h->nlmsg_type);
+                return -1;
+            }
+
+            parse_diag_msg(NLMSG_DATA(h), h->nlmsg_len);
+        }
+    }
+}
+
+
 int main(int argc, char *argv[]){
     int nl_sock = 0, numbytes = 0, rtalen = 0;
     struct nlmsghdr *nlh;
@@ -272,34 +276,19 @@ int main(int argc, char *argv[]){
         return EXIT_FAILURE;
     }
 
-    //Send the request for the sockets we are interested in
-    if(send_diag_msg(nl_sock) < 0){
-        perror("sendmsg: ");
-        return EXIT_FAILURE;
-    }
+    int ret = 0;
 
-    //The requests can (will in most cases) come as multiple netlink messages. I
-    //need to receive all of them. Assumes no packet loss, so if the last packet
-    //(the packet with NLMSG_DONE) is lost, the application will hang.
-    while(1){
-        numbytes = recv(nl_sock, recv_buf, sizeof(recv_buf), 0);
-        nlh = (struct nlmsghdr*) recv_buf;
+    for (;;) {
 
-        while(NLMSG_OK(nlh, numbytes)){
-            if(nlh->nlmsg_type == NLMSG_DONE)
-                return EXIT_SUCCESS;
+      int ret = send_query(nl_sock) || receive_responses(nl_sock);
 
-            if(nlh->nlmsg_type == NLMSG_ERROR){
-                fprintf(stderr, "Error in netlink message\n");
-                return EXIT_FAILURE;
-            }
+      if (ret != 0) {
+        close(nl_sock);
+        return ret;
+      }
 
-            diag_msg = (struct inet_diag_msg*) NLMSG_DATA(nlh);
-            rtalen = nlh->nlmsg_len - NLMSG_LENGTH(sizeof(*diag_msg));
-            parse_diag_msg(diag_msg, rtalen);
+      sleep(1);
 
-            nlh = NLMSG_NEXT(nlh, numbytes); 
-        }
     }
 
     return EXIT_SUCCESS;
